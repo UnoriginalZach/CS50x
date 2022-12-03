d6a031b7d6fb7a21f1ee09beef3e928d074d1d18
zach f <lofi.it.guy@gmail.com>
2022-12-02 21:14:27 -0800

    second problem in problem set 1 created

---
+++ probset1/Credit.java
@@ -0,0 +1,9 @@
+public class Credit {
+    public static void main(String[] args) {
+        int cardNumber = 0;
+        int multipliedSum = 0;
+        int otherSum = 0;
+        String number = String.valueOf(cardNumber);
+        char[] digits = number.toCharArray();
+    }
+}
